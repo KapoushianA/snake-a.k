bda76a11c395c9e802b92c4d3015b4f8a4e27f15
KapoushianA <armen.goml@yahoo.com>
2014-10-07 10:47:16 -0700

    stuff

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -1,7 +1,30 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+var snake;
+
+var context;
+var screenWidth;
+var screenHeight;
+
+gameInitialize();
+gameDraw();
+
+function gameInitialize() {
+    var canvas= document.getElementById("game-screen");
+    context = canvas.getContext("2d");
+    
+    screenWidth = window.innerWidth;
+    screenHeight = window.innerHeight;
+    
+    canvas.width = screenWidth;
+    canvas.height =screenHeight;
+}
+
+function gameLoop() {
+    
+}
+
+function gameDraw() {
+    context.fillStyle ="rgb(158, 237, 255)";
+    context.fillRect(0, 0, screenWidth, screenHeight);
+}
 
 
